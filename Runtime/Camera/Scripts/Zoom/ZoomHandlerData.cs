b04195e3a9ddeb08f0fbebfec05f24ce1a4691a7
CI-CD-RLTY <cicdunity@rlty.live>
2023-01-16 11:42:46 +0000

    CI auto-update from Plastic changeset 574

--- Runtime/Camera/Scripts/Zoom/ZoomHandlerData.cs
+++ Runtime/Camera/Scripts/Zoom/ZoomHandlerData.cs
@@ -1,18 +1,16 @@
 using UnityEngine.Events;
 
-namespace _0_MetaEvent._0_Features.Camera.Scripts.Zoom
+
+public static class ZoomHandlerData
 {
-    public static class ZoomHandlerData
-    {
-        public static UnityAction OnZoomStart;
-        public static UnityAction OnZoomEnd;
-        public static UnityAction OnUnzoomStart;
-        public static UnityAction OnUnzoomEnd;
+    public static UnityAction OnZoomStart;
+    public static UnityAction OnZoomEnd;
+    public static UnityAction OnUnzoomStart;
+    public static UnityAction OnUnzoomEnd;
 
-        public static void ZoomStart() => OnZoomStart?.Invoke();
-        public static void ZoomEnd() => OnZoomEnd?.Invoke();
+    public static void ZoomStart() => OnZoomStart?.Invoke();
+    public static void ZoomEnd() => OnZoomEnd?.Invoke();
 
-        public static void UnzoomStart() => OnUnzoomStart?.Invoke();
-        public static void UnzoomEnd() => OnUnzoomEnd?.Invoke();
-    }
+    public static void UnzoomStart() => OnUnzoomStart?.Invoke();
+    public static void UnzoomEnd() => OnUnzoomEnd?.Invoke();
 }
