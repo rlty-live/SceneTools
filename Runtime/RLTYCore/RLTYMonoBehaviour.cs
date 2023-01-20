aa4de69719b09e0443b82af62db5f346be56102f
CI-CD-RLTY <cicdunity@rlty.live>
2023-01-20 12:14:35 +0000

    CI auto-update from Plastic changeset 717

--- Runtime/RLTYCore/RLTYMonoBehaviour.cs
+++ Runtime/RLTYCore/RLTYMonoBehaviour.cs
@@ -69,5 +69,88 @@ public abstract class RLTYMonoBehaviourBase : JMonoBehaviour
                 Destroy(gameObject);
 #endif
     }
+
+    /// <summary>
+    /// RLTY's overload of Unity's Debug function, only activated in Debug build, inEditor display depends on the inherited "debug" boolean.
+    /// </summary>
+    /// <param name="message"> Message to be displayed in the console </param>
+    /// <param name="context"> Identical to context Debug's context paramater</param>
+    /// <param name="type"> Unity's Debug.Type</param>
+    public virtual void RLTYLog(string message, Object context, LogType type)
+    {
+#if !UNITY_EDITOR
+        if (Debug.isDebugBuild) debug = true;
+        else debug = false;
+#endif
+
+        string formattedMessage = "<b>[" + context.GetType().ToString() + "]</b>" + message;
+
+        switch (type)
+        {
+            case LogType.Log:
+                if (debug) Debug.Log(formattedMessage, context);
+
+                break;
+            case LogType.Error:
+                if (debug) Debug.LogError(formattedMessage, context);
+
+                break;
+            case LogType.Warning:
+                if (debug) Debug.LogWarning(formattedMessage, context);
+
+                break;
+            case LogType.Assert:
+                Debug.LogError("Assertions are not yet supported, please use the regular unity one");
+
+                break;
+            case LogType.Exception:
+                Debug.LogError("Exceptions are not yet supported, please use the regular unity one");
+
+                break;
+
+        }
+    }
+
+    /// <summary>
+    /// RLTY's overload of Unity's Debug function, only activated in Debug build, inEditor display depends on the inherited "debug" boolean.
+    /// </summary>
+    /// <param name="message"> Message to be displayed in the console </param>
+    /// <param name="context"> Identical to context Debug's context paramater</param>
+    /// <param name="type"> Unity's Debug.Type</param>
+    /// <param name="color"> Color Display in editor </param>
+    public virtual void RLTYLog(string message, Object context, LogType type, Color color)
+    {
+#if !UNITY_EDITOR
+        if (Debug.isDebugBuild) debug = true;
+        else debug = false;
+#endif
+        string hexValue = ColorUtility.ToHtmlStringRGBA(color);
+        string formattedMessage =
+            "<color = " + hexValue + ">" + "<b>" + "[" + context.GetType().ToString() + "]</b> </color>" + message;
+
+        switch (type)
+        {
+            case LogType.Log:
+                if (debug) Debug.Log(formattedMessage, context);
+
+                break;
+            case LogType.Error:
+                if (debug) Debug.LogError(formattedMessage, context);
+
+                break;
+            case LogType.Warning:
+                if (debug) Debug.LogWarning(formattedMessage, context);
+
+                break;
+            case LogType.Assert:
+                Debug.LogError("Assertions are not yet supported, please use the regular unity one");
+
+                break;
+            case LogType.Exception:
+                Debug.LogError("Exceptions are not yet supported, please use the regular unity one");
+
+                break;
+        }
+    }
     #endregion
 }
