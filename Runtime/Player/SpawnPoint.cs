9890e278d3f402582169cd42018a791552aed59c
CI-CD-RLTY <cicdunity@rlty.live>
2023-01-31 16:08:53 +0000

    CI auto-update from Plastic changeset 987

--- Runtime/Player/SpawnPoint.cs
+++ Runtime/Player/SpawnPoint.cs
@@ -4,7 +4,7 @@ namespace Judiva.Metaverse.Interactions
 {
     public class SpawnPoint : MonoBehaviour
     {
-
+        public static float spawnRadius = 5;
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.cyan;
