247180cfc6bcdc5624739e79c864d12a23c6fdb8
CI-CD-RLTY <cicdunity@rlty.live>
2023-03-07 15:51:10 +0000

    CI auto-update from Plastic changeset 1886

--- Runtime/Customizer/Scripts/SceneDescription.cs
+++ Runtime/Customizer/Scripts/SceneDescription.cs
@@ -36,7 +36,7 @@ namespace RLTY.Customisation
         public string staticFramesAddress;
         public string backendApiHost;
 
-        public string initialAvatar;
+        public AvatarData initialAvatar;
 
         public string ResolvedDnsName {  get { return string.IsNullOrEmpty(dnsName) ? "localhost" : dnsName; } }
 
@@ -140,6 +140,15 @@ namespace RLTY.Customisation
             public string token;
             public uint id;
             public string channelName;
+        }
+
+        [System.Serializable]
+        public class AvatarData
+        {
+            public string id;
+            public string type;
+            public string skin;
+            public bool customReadyPlayerMe = false;
         }
     }
 
