f40b4a8fc4dd937710655ceed8e90e7f8296d8a4
CI-CD-RLTY <cicdunity@rlty.live>
2023-05-29 10:42:41 +0000

    CI auto-update from Plastic changeset 3004

--- Runtime/VoiceManagerHandlerData.cs
+++ Runtime/VoiceManagerHandlerData.cs
@@ -7,7 +7,7 @@ public static class VoiceManagerHandlerData
     public static event Action OnStart, OnQuit, OnJoinGlobalAudioChat, OnLeaveGlobalAudioChat;
     public static event Action<string> OnJoinVisio, OnLeaveVisio;
     public static event Action<bool> OnTalk;
-    public static event Action<bool> OnMute, OnMicrophoneAvailable;
+    public static event Action<bool> OnMute, OnMicrophoneAvailable, OnMuteLocalMicrophone;
 
     #region Methods for manager implementing voice
     public static void NotifyOnTalk(bool state) => OnTalk?.Invoke(state);
@@ -20,6 +20,7 @@ public static class VoiceManagerHandlerData
     public static void NotifyOnJoinVisio(string id) => OnJoinVisio?.Invoke(id);
     public static void NotifyOnLeaveVisio(string id) => OnLeaveVisio?.Invoke(id);
     public static void NotifyMute(bool state) => OnMute?.Invoke(state);
+    public static void NotifyMuteLocalMicrophone(bool state) => OnMuteLocalMicrophone?.Invoke(state);
     public static void NotifyMicrophoneAvailable(bool state) => OnMicrophoneAvailable?.Invoke(state);
 
     #endregion
