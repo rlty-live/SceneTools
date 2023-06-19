dea4d309de0dc4c6094c7fe081a4b264be3e0d31
CI-CD-RLTY <cicdunity@rlty.live>
2023-06-19 17:27:07 +0000

    CI auto-update from Plastic changeset 3361

--- Runtime/Scripts/Player/IPlayer.cs
+++ Runtime/Scripts/Player/IPlayer.cs
@@ -63,6 +63,11 @@ public static class AllPlayers
             Me = null;
         OnPlayerLeft?.Invoke(player);
     }
+
+    public static void ClearLastKnowPlayerPosition()
+    {
+        _lastKnownPositionExists = false;
+    }
 }
 
 /// <summary>
