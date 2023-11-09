e59866474f9145f1ab082ec12d9a36bbf9f06bbf
CI-CD-RLTY <cicdunity@rlty.live>
2023-11-09 21:02:39 +0000

    CI auto-update from Plastic changeset 5309

--- Runtime/Scripts/Portal/PortalData.cs
+++ Runtime/Scripts/Portal/PortalData.cs
@@ -1,8 +1,20 @@
-﻿namespace Portal
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Portal
 {
     public class PortalData : NetworkSceneTool
     {
         public string URL;
         public bool StartActive;
+        
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+
+            Gizmos.DrawCube(transform.position + (Vector3.up * 0.75f), new Vector3(1.5f, 1.5f, 0.1f));
+        }
+#endif
     }
 }
