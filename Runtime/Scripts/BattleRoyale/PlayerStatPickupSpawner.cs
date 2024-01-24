96f19af110d57b6c686d6eed6a02aeb298260355
CI-CD-RLTY <cicdunity@rlty.live>
2024-01-24 22:29:39 +0000

    CI auto-update from Plastic changeset 5741

---
+++ Runtime/Scripts/BattleRoyale/PlayerStatPickupSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace GameStationData
+{
+    [System.Serializable]
+    public enum PlayerStatPickupType
+    {
+        Health,
+        Shield
+    }
+    
+    public class PlayerStatPickupSpawner : SceneTool
+    {
+        public PlayerStatPickupType PlayerStatPickupType;
+
+        protected override void DrawGizmos()
+        {
+            Gizmos.color = PlayerStatPickupType == PlayerStatPickupType.Health ? new Color(1, 0.2f, 0.2f) : new Color(0, 1, 1);
+            Matrix4x4 DefaultMatrix = Gizmos.matrix;
+            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+            Gizmos.DrawWireCube(Vector3.zero, new Vector3(1f, 0.1f, 1f));
+            Gizmos.DrawWireSphere(Vector3.up, 0.2f);
+            Gizmos.matrix = DefaultMatrix;
+        }
+    }
+}
